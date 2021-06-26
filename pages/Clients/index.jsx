f09b68981d74ba9d4e7e9df74bd17ecca8ab1112
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2021-06-26 18:39:09 +0500

    added rouring

---
+++ pages/Clients/index.jsx
@@ -0,0 +1,10 @@
+const Test = () => {
+  console.log("test");
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  );
+};
+
+export default Test;
---
+++ pages/about.jsx
@@ -0,0 +1,22 @@
+console.log("about");
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt
+        soluta laborum modi animi iure, laudantium assumenda enim obcaecati
+        quisquam, architecto sint ratione at maxime illum, aspernatur fuga.
+        Aperiam, dolorum pariatur.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt
+        soluta laborum modi animi iure, laudantium assumenda enim obcaecati
+        quisquam, architecto sint ratione at maxime illum, aspernatur fuga.
+        Aperiam, dolorum pariatur.
+      </p>
+    </div>
+  );
+};
+
+export default About;
